3b6007a1ad5057e947229af805c4b007133feeaf
wangrl2016 <wangrl2016@gmail.com>
2023-04-11 20:30:01 +0800

    Audio file writer

--- media/filters/audio_file_writer.cc
+++ media/filters/audio_file_writer.cc
@@ -5,5 +5,22 @@
 #include "media/filters/audio_file_writer.h"
 
 namespace media {
+    AudioFileWriter::AudioFileWriter() : pts_(0) {
 
+    }
+
+    AudioFileWriter::~AudioFileWriter() {
+
+    }
+
+    bool AudioFileWriter::Open(const std::string& file_path,
+                               const AudioParameters& input_audio_parameters,
+                               const AudioParameters& output_audio_parameters) {
+        int error;
+
+        // Open the output file to write to it.
+
+
+        return true;
+    }
 }
--- media/filters/audio_file_writer.h
+++ media/filters/audio_file_writer.h
@@ -4,11 +4,27 @@
 
 #pragma once
 
+#include <string>
 #include "base/memory/noncopyable.h"
+#include "media/base/audio_parameters.h"
 
 namespace media {
     class AudioFileWriter : public base::Noncopyable {
     public:
+        AudioFileWriter();
 
+        ~AudioFileWriter();
+
+        bool Open(const std::string& file_path,
+                  const AudioParameters& input_audio_parameters,
+                  const AudioParameters& output_audio_parameters);
+
+        void Close();
+
+    private:
+        // Timestamp for the audio frames.
+        int64_t pts_;
+        AudioParameters input_audio_parameters_;
+        AudioParameters output_audio_parameters_;
     };
 }
