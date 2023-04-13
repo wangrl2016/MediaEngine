6f1acffba544c6406444e2cec7f26c1b7dcd9042
wangrl2016 <wangrl2016@gmail.com>
2023-04-13 19:57:07 +0800

    FFmpeg open decoder

--- media/filters/audio_file_reader.cc
+++ media/filters/audio_file_reader.cc
@@ -76,10 +76,49 @@ namespace media {
 
         if (!found_stream)
             return false;
+
+        // get the codec context
+        codec_context_ = std::unique_ptr<AVCodecContext, ScopedPtrAVFreeContext>(
+                avcodec_alloc_context3(nullptr));
+        if (avcodec_parameters_to_context(codec_context_.get(),
+                                          format_context->streams[stream_index_]->codecpar) < 0) {
+            return false;
+        }
+
+        DCHECK_EQ(codec_context_->codec_type, AVMEDIA_TYPE_AUDIO);
+        return true;
     }
 
     bool AudioFileReader::OpenDecoder() {
+        const AVCodec* codec = avcodec_find_decoder(codec_context_->codec_id);
+        if (codec) {
+            // Mp3 decodes to S16P which we don't support, tell it to use S16 instead.
+            if (codec_context_->sample_fmt == AV_SAMPLE_FMT_S16P)
+                codec_context_->request_sample_fmt = AV_SAMPLE_FMT_S16;
 
+            const int result = avcodec_open2(codec_context_.get(), codec, nullptr);
+            if (result < 0) {
+                DLOG(WARNING) << "AudioFileReader::Open(): could not open codec - result: " << result;
+                return false;
+            }
+
+            // Ensure avcodec_open2() respected our format request.
+            if (codec_context_->sample_fmt == AV_SAMPLE_FMT_S16P) {
+                DLOG(ERROR) << "AudioFileReader::Open() : unable to configure a"
+                            << " supported sample format - "
+                            << codec_context_->sample_fmt;
+                return false;
+            }
+        } else {
+            DLOG(WARNING) << "AudioFileReader::Open() : could not find codec.";
+            return false;
+        }
+
+        // Store initial values to guard against midstream configuration changes.
+        input_audio_parameters_.set_channel_count(codec_context_->channels);
+        input_audio_parameters_.set_sample_rate(codec_context_->sample_rate);
+        input_audio_parameters_.set_av_sample_format(codec_context_->sample_fmt);
+        return true;
     }
 
     bool AudioFileReader::ReadPacket(AVPacket* output_packet) {
