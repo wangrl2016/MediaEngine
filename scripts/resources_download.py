4308873657272d21781cf64cf815126253241114
wangrl2016 <wangrl2016@gmail.com>
2023-04-22 13:33:41 +0800

    Audio download

--- scripts/resources_download.py
+++ scripts/resources_download.py
@@ -1,2 +1,21 @@
+import os.path
+import shutil
+import urllib.request
 
+audio_urls = {
+    'https://www.chosic.com/wp-content/uploads/2021/09/Feel-Good.mp3',
+}
 
+audio_dir = 'resources/audio/'
+
+if __name__ == '__main__':
+    # download audio
+    for url in audio_urls:
+        filename = url.split('/')[-1]
+
+        local_path = os.path.join(audio_dir, filename)
+        print('Downloading ' + filename)
+
+        request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        with urllib.request.urlopen(request) as response, open(local_path, 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
