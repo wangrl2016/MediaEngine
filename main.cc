35f8a4bbf792f92930e2321e454aa9e644be0aa8
wangrl2016 <wangrl2016@gmail.com>
2023-04-23 11:43:58 +0800

    Add gflags

--- main.cc
+++ main.cc
@@ -4,6 +4,13 @@
 
 #include <cstdlib>
 
+#include <gflags/gflags.h>
+
+DEFINE_string(intput, "", "lottie animation to render");
+DEFINE_string(output, "", "mp4 file to create");
+DEFINE_string(asset, "", "path to assets needed for json file");
+DEFINE_int32(fps, 25, "video frames per second");
+
 int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
