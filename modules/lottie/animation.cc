647707af4868cb5eabbada197f151189cab24ebc
wangrl2016 <wangrl2016@gmail.com>
2023-04-28 10:23:36 +0800

    Lottie animation

---
+++ modules/lottie/animation.cc
@@ -0,0 +1,7 @@
+//
+// Created by wangrl2016 on 2023/4/28.
+//
+
+#include "modules/lottie/animation.h"
+
+
---
+++ modules/lottie/animation.h
@@ -0,0 +1,13 @@
+//
+// Created by wangrl2016 on 2023/4/28.
+//
+
+#pragma once
+
+namespace modules {
+    class Animation {
+
+    };
+}
+
+
